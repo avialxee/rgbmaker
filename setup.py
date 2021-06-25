7aaadfa025f8b337b23949f3d876d61b5b0a0250
Avinash Kumar <avialxee@users.noreply.github.com>
2021-06-25 16:56:05 +0530

    0.0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as rdme:
 
 setup(
     name = 'rgbmaker',
-    version = '0.0.4.2',
+    version = '0.0.4.3',
     url='https://github.com/avialxee/rgbmaker',
     author='Avinash Kumar',
     author_email='avialxee@gmail.com',
