10c0e6c4224a35cad7dd7b048b9bd79098b3dfe3
Ninisha224 <120358689+Ninisha224@users.noreply.github.com>
2023-01-11 20:23:44 +0530

    Update cli.py

--- src/rgbmaker/cli.py
+++ src/rgbmaker/cli.py
@@ -50,7 +50,7 @@ def cli():
     S_e=list(map(float, (args.flux_error or "1, 1").split(',')))
     freq=list(map(float, (args.freq_list or "150, 1420").split(',')))
     #SPEC_INDEX=args.spec_index
-    if position == None:
+    if S:
         #print(S)
         #print(type(S[0]))
         sindex = pl_powerlawsi(S,S_e,freq, kind='png', label="output")
